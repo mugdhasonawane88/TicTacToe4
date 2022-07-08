b1ae968230dfcfc7e9fb912ddc353e9f96ea82fe
mugdha <mugdhasonawane88@gmail.com>
2022-07-08 09:11:01 +0530

    feat: display board with 3x3 squares

--- src/components/TicTacToe.test.js
+++ src/components/TicTacToe.test.js
@@ -6,7 +6,20 @@ describe('TicTacToe component', () => {
     render(<TicTacToe />);
 
     const headerElement = screen.getByTestId('header');
+
     expect(headerElement).toBeInTheDocument();
     expect(headerElement.textContent).toBe('Tic Tac Toe Game');
   });
+
+  test('Should create empty nine squares in the board when game starts', () => {
+    render(<TicTacToe />);
+
+    const squares = screen.queryAllByRole('square');
+
+    expect(squares).toHaveLength(9);
+    squares.forEach((square) => {
+      expect(square.textContent).toBe('');
+    })
+  });
+
 });
